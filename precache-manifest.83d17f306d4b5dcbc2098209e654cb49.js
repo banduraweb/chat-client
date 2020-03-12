85257da4411661c7b747debfa88064f924b3077a
bandura <banduraweb@ukr.net>
2020-03-12 05:14:02 +0200

    Updates

---
+++ precache-manifest.83d17f306d4b5dcbc2098209e654cb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790c889aa61f28af4313015a2aa5fc01",
+    "url": "/chat-client/index.html"
+  },
+  {
+    "revision": "eefa11f3139dcfee290b",
+    "url": "/chat-client/static/css/main.b97b2eba.chunk.css"
+  },
+  {
+    "revision": "22d38b307ed95e4f7abf",
+    "url": "/chat-client/static/js/2.08d53f1c.chunk.js"
+  },
+  {
+    "revision": "52e18199ebeaf42c97521a3f15c4f9ff",
+    "url": "/chat-client/static/js/2.08d53f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eefa11f3139dcfee290b",
+    "url": "/chat-client/static/js/main.20833305.chunk.js"
+  },
+  {
+    "revision": "5ec6b33ea031f588a49b",
+    "url": "/chat-client/static/js/runtime-main.ef98a6cb.js"
+  }
+]);
